7a1437a93f9cb0bacd89363c6f45f7f6ffdbb0d7
Floris van Doorn <fpvdoorn@gmail.com>
2024-04-11 19:31:14 +0200

    update doc config

--- lakefile.lean
+++ lakefile.lean
@@ -29,6 +29,11 @@ package «bonnAnalysis» where
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
+-- This is run only if we're in `dev` mode. This is so not everyone has to build doc-gen
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
+
 @[default_target]
 lean_lib «BonnAnalysis» where
   moreLeanArgs := moreLeanArgs
