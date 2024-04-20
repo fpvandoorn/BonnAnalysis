8223f9cf0e79c9fd60d2c190254abf19937b3b85
Stefan Albrecht <s26salbr@uni-bonn.de>
2024-04-20 16:50:55 +0200

    finished instance NormedAddCommGroup (V \to12 E)

--- BonnAnalysis/Plancherel.lean
+++ BonnAnalysis/Plancherel.lean
@@ -53,16 +53,42 @@ theorem snorm_fourierIntegralInv {f : V → E} (hf : Integrable f) (h2f : Memℒ
 scoped[MeasureTheory] notation:25 α " →₁₂[" μ "] " E =>
     ((α →₁[μ] E) ⊓ (α →₂[μ] E) : AddSubgroup (α →ₘ[μ] E))
 
-
 /- Note: `AddSubgroup.normedAddCommGroup` is almost this, but not quite. -/
 instance : NormedAddCommGroup (V →₁₂[volume] E) :=
-  AddGroupNorm.toNormedAddCommGroup sorry
+  AddGroupNorm.toNormedAddCommGroup {
+    toFun := fun ⟨f,_⟩ ↦ ENNReal.toReal <| snorm f 2 volume
+    map_zero' := by simp [snorm_congr_ae AEEqFun.coeFn_zero, snorm_zero]
+    add_le' := fun ⟨f, _, hf⟩ ⟨g, _, hg⟩ ↦ ENNReal.toReal_le_add (by
+        simp [snorm_congr_ae (AEEqFun.coeFn_add f g),
+              snorm_add_le ((Lp.mem_Lp_iff_memℒp.1 hf).1) ((Lp.mem_Lp_iff_memℒp.1 hg).1)])
+      ((Lp.mem_Lp_iff_snorm_lt_top.1 hf).ne) ((Lp.mem_Lp_iff_snorm_lt_top.1 hg).ne)
+    neg' := by simp[snorm_congr_ae (AEEqFun.coeFn_neg _)]
+    eq_zero_of_map_eq_zero' := by
+      intro ⟨f, _, hf⟩ h
+      simp [ENNReal.toReal_eq_zero_iff] at h
+      rcases h with h | h; swap
+      · absurd h; exact (Lp.mem_Lp_iff_snorm_lt_top.1 hf).ne
+      ext
+      apply ae_eq_trans <| (snorm_eq_zero_iff ((Lp.mem_Lp_iff_memℒp.1 hf).1) (by simp)).1 h
+      apply ae_eq_trans (Lp.coeFn_zero E 2 volume).symm; rfl
+  }
+
 instance : NormedSpace ℝ (V →₁₂[volume] E) := sorry
 
 
 /- The Fourier integral as a continuous linear map `L^1(V, E) ∩ L^2(V, E) → L^2(V, E)`. -/
-def fourierIntegralL2OfL12 : (V →₁₂[volume] E) →L[ℝ] (V →₂[volume] E) :=
-    sorry
+def fourierIntegralL2OfL12Fun : (V →₁₂[volume] E) → (V →₂[volume] E) :=
+  fun ⟨f,hf,hf2⟩ ↦ (memℒp_fourierIntegral (memℒp_one_iff_integrable.1 <|
+      Lp.mem_Lp_iff_memℒp.1 hf) (Lp.mem_Lp_iff_memℒp.1 hf2)).toLp <| 𝓕 f
+
+def fourierIntegralL2OfL12 : (V →₁₂[volume] E) →L[ℝ] (V →₂[volume] E) := sorry
+  /-have : IsBoundedLinearMap ℝ fourierIntegralL2OfL12Fun := {
+    map_add := sorry
+    map_smul := sorry
+    bound := sorry
+  }
+  IsBoundedLinearMap.toContinuousLinearMap this-/
+
 
 
 /- The Fourier integral as a continuous linear map `L^2(V, E) → L^2(V, E)`. -/
